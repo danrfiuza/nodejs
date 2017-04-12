c9af8f83d71a265255682d94cde6fbdd2a0e738e
danrfiuza <danielfiuza01@gmail.com>
2017-04-12 18:42:27 -0300

    capitulo 4 terminado

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,17 +1,17 @@
-var express = require('express'),
-    routes = require('./routes');
-var app = express();
-load = require('express-load'),
-    app = express();
+var express = require('express')
+, load = require('express-load')
+, app = express();
+// ...stack de configurações do servidor...
+load('models')
+.then('controllers')
+.then('routes')
+.into(app);
+// ...app.listen(3000)...
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.get('/', routes.index);
-app.get('/usuarios', routes.user.index);
-load('models')
-    .then('controllers')
-    .then('routes')
-    .into(app);
-app.listen(3000, function() {
-    console.log("Ntalk no ar.");
+//app.get('/', routes.index);
+//app.get('/usuarios', routes.user.index);
+app.listen(3000, function(){
+console.log("Ntalk no ar.");
 });
