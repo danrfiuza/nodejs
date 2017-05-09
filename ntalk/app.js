3101c2b73df55820a1aace614b0ff2f57408ac08
danrfiuza <danielfiuza01@gmail.com>
2017-05-09 17:46:52 -0300

    cap 5

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,17 +1,25 @@
-var express = require('express')
-, load = require('express-load')
-, app = express();
+var express = require('express'),
+    load = require('express-load'),
+    error = require('./middleware/error'),
+    app = express();
 // ...stack de configurações do servidor...
 load('models')
-.then('controllers')
-.then('routes')
-.into(app);
+    .then('controllers')
+    .then('routes')
+    .into(app);
 // ...app.listen(3000)...
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+app.use(error.notFound);
+app.use(error.serverError);
 //app.get('/', routes.index);
 //app.get('/usuarios', routes.user.index);
-app.listen(3000, function(){
-console.log("Ntalk no ar.");
+app.listen(3000, function() {
+    console.log("Ntalk no ar.");
 });
--- ntalk/controllers/home.js
+++ ntalk/controllers/home.js
@@ -1,8 +1,31 @@
-module.exports = function(app){
-	var HomeController ={
-		index: function(req,res){
-			res.render('home/index');
-		}
-	};
-	return HomeController;
+module.exports = function(app) {
+    //var Usuario = app.models.usuario;
+
+    var HomeController = {
+        index: function(req, res) {
+            res.render('home/index');
+        },
+
+        //Action login
+        login: function(req, res) {
+            var email = req.body.usuario.email;
+            var nome = req.body.usuario.nome;
+
+            if (email && nome) {
+                var usuario = req.body.usuario;
+                usuario['contatos'] = [];
+                req.session.usuario = usuario;
+                req.redirect('/contatos');
+            } else {
+                res.redirect('/');
+            }
+        },
+        //Action logout
+        logout: function(req, res) {
+            req.session.destroy();
+            res.redirect('/');
+        }
+    };
+
+    return HomeController;
 };
