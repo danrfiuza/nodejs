c37f8d2a44bbc2d28002b7c6927a2cd12b7fd2d3
danrfiuza <danielfiuza01@gmail.com>
2017-05-09 17:55:55 -0300

    installed socket io

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,7 +1,9 @@
 var express = require('express'),
+    app = express(),
     load = require('express-load'),
     error = require('./middleware/error'),
-    app = express();
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 // ...stack de configurações do servidor...
 load('models')
     .then('controllers')
@@ -20,6 +22,13 @@ app.use(error.notFound);
 app.use(error.serverError);
 //app.get('/', routes.index);
 //app.get('/usuarios', routes.user.index);
+io.sockets.on('connection', function(client) {
+    client.on('send-server', function(data) {
+        var msg = "<b>" + data.nome + ":</b> " + data.msg + "<br>";
+        client.emit('send-client', msg);
+        client.broadcast.emit('send-client', msg);
+    });
+});
 app.listen(3000, function() {
     console.log("Ntalk no ar.");
 });
